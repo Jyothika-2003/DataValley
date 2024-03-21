8446335c60e85ca86a14893fa154c6499ec2ee8c
Jyothika-2003 <113087132+Jyothika-2003@users.noreply.github.com>
2024-03-21 16:18:13 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,4 +1,5 @@
-/*Write a java program to create linkedlist with employee objects, sort the all object based name of employee and salary of employee. Note: take Employee as a class with id, name, designation and salary fields*/
+/*Write a java program to create linkedlist with employee objects, sort the all object based name of employee and salary of employee. 
+Note: take Employee as a class with id, name, designation and salary fields*/
 
 /*-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
